7803ac19aae50a4c7a10a829079b63800b3e63b4
Sebastian Ortiz <dsebaortiz@gmail.com>
2021-12-30 16:33:56 -0300

    Change link home logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import { Link } from 'react-router-dom';
 import Cart from './Cart';
 import CartIcon from '../icons/cart.svg';
 
@@ -13,12 +14,12 @@ const Navbar = ({cart, totalItems, onEmptyCart}) => {
   return (
     <header className="text-gray-600 body-font shadow-md">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Link to="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-gray-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
           <span className="ml-3 text-xl">MusikStore</span>
-        </a>
+        </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           {/* <a className="mr-5 hover:text-gray-900">Link</a> */}
         </nav>
